ea9637110b7f4a7576643e8f10e31d7d640b604f
lamine450 <62139787+lamine450@users.noreply.github.com>
2020-04-15 04:38:03 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUIZZ</title>
+    <link rel="stylesheet" href="./public/css/quizz.css">
+</head>
+<body>
+<div class="header"></div>
+    <div class="logo"></div>
+    <div class="header-text">Plaisir de jouer</div>
+<div class="content">
+<?php
+
+        require_once("./page/connexion.php");
+
+?>
+
+</div>  
+</body>
+</html>
