c4cb6f6b5f337828e7c006a671278d53e9de65df
lamine450 <62139787+lamine450@users.noreply.github.com>
2020-04-15 04:43:19 +0000

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,27 @@
+<div class="container">
+    <div class="container-header">
+        <div class="title">Login Forme</div>
+    </div>
+    <div class="container-body">
+        <form action="">
+            <div class="input-form">
+                <div class="icon-form icon-form-login"></div>
+                <input type="text"class="form-control" name="" id="" placeholder="Login">
+                <div class="error-form"></div>
+            </div>
+
+            <div class="input-form">
+                <div class="icon-form icon-form-pwd"></div>
+                <input type="password"class="form-control" name="" id="" placeholder="Password">
+                <div class="error-form"></div>
+            </div>
+
+            <div class="input-form">
+                
+                <input button="submit"class="btn-form" name="" id="">connexion</button>
+                <a href="" class="link-form">S'inscrire pour jouer</a>
+            </div>
+
+        </form>
+    </div>
+</div>
