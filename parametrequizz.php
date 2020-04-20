65b1bc17823fc95668bd5e0f177c494933da79dc
lamine450 <62139787+lamine450@users.noreply.github.com>
2020-04-20 00:06:19 +0000

    Add files via upload

---
+++ parametrequizz.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRÉER ET PARAMÉRTER VOS QUIZZ</title>
+    <link rel="stylesheet" href="./styles/quizz.css">
+</head>
+<body>
+            <div class="header"></div>
+            <div class="logo"></div>
+            <div class="header-text">Plaisir de jouer</div>
+    <div class="content">
+    </div>  
+</body>
+</html>
+
+
+<div class="container">
+    <div class="container-header">
+        <div class="title">CRÉER ET PARAMÉRTER VOS QUIZZ</div>
+             <div class="input-form">
+                <input button="submit"class="btn-form" name="" id="">Déconnexion</button>
+            </div>
+    </div>
+    <div class="container-body1">                
+        <form action="" method="post" id="form-AAABB">
+            <div class="input-liste question">
+                <div class="icon-form icon-form-Créer Admin"></div>
+                    <div class="icon-form icon-form-Liste joueurs"></div>
+                    <div class="icon-form icon-form-Créer Questions"></div>
+                <input type="text"class="form-control" error="error-1" name="" id="" placeholder="Login">
+                <div class="error-form"id="error-1"></div>
+            </div>
+
+            <div class="input-form">
+                <div class="icon-form icon-form-AAABB"></div>
+                <input type="Liste Questions"class="form-control" error="error-2" name="" id="" placeholder="Liste Questions">
+                <input type="Créer Admin"class="form-control" error="error-2" name="" id="" placeholder="Créer Admin">
+                <input type="Liste joueurs"class="form-control" error="error-2" name="" id="" placeholder="Liste joueurs">
+                <input type="Créer Questions"class="form-control" error="error-2" name="" id="" placeholder="Créer Questions">
+                <div class="error-form" id="error-2"></div>
+            </div>
+
+            
+
+        </form>
+    </div>
+</div>
+        </form>
+    </div>
+</div>
+</body>
+</html>
